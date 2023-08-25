4724b41d34392cb0d24b908032b5efd9aff86b90
ro-el-c <chung3286@naver.com>
2023-08-25 18:14:31 +0900

    chore: project setting

--- src/main/java/sideproject/daangn/DaangnApplication.java
+++ src/main/java/sideproject/daangn/DaangnApplication.java
@@ -8,6 +8,7 @@ public class DaangnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaangnApplication.class, args);
+		System.out.println("Hello, Daangn!");
 	}
 
 }
